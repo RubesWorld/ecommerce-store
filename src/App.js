966d4f987bb27a0e5530e9024ed4af9c9768a779
RubesWorld <hi@rubesworld.live>
2021-10-26 21:24:31 -0700

    made product code modular

--- src/App.js
+++ src/App.js
@@ -16,43 +16,7 @@ function App() {
       <main>
         <div>
           {data.products.map((product) => (
-            <div>
-              <div className="row center">
-                <div className="card">
-                  <a href={`/product/${product.id}`}>
-                    <img
-                      className="medium"
-                      src={product.image}
-                      alt={product.name}
-                    />
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/${product.id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
-              </div>
-              {/* <ProductCard key={product.id} product={product} /> */}
-            </div>
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </main>
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,40 +1,19 @@
 import React from "react";
+import Rating from "./Rating.js";
 
 function ProductCard({ product }) {
   return (
-    <div>
-      <div className="row center">
-        <div className="card">
+    <div className="row center">
+      <div key={product.id} className="card">
+        <a href={`/product/${product.id}`}>
+          <img className="medium" src={product.image} alt="product-pants" />
+        </a>
+        <div className="card-body">
           <a href="product.html">
-            <img
-              className="medium"
-              src="./images/product-1.jpg"
-              alt="product-pants"
-            />
+            <h2>{product.name}</h2>
           </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>{product.products.name}</h2>
-            </a>
-            <div className="rating">
-              <span>
-                <i className="fa fa-star"></i>
-              </span>
-              <span>
-                <i className="fa fa-star"></i>
-              </span>
-              <span>
-                <i className="fa fa-star"></i>
-              </span>
-              <span>
-                <i className="fa fa-star"></i>
-              </span>
-              <span>
-                <i className="fa fa-star"></i>
-              </span>
-            </div>
-            <div className="price">{product.products.price}</div>
-          </div>
+          <Rating rating={product.rating} numReviews={product.numReviews} />
+          <div className="price">{product.price}</div>
         </div>
       </div>
     </div>
