99553cc9775a991d8c2a815ae228ed8b3ab5c6a9
RubesWorld <hi@rubesworld.live>
2021-10-30 15:32:36 -0700

    Added useEffect to bring in data

--- frontend/src/Pages/HomeScreen.js
+++ frontend/src/Pages/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import ProductCard from "../Components/ProductCard.js";
 //data import
 import data from "../data.js";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([])
+  
+  useEffect(() => {
+   const fetchData = async () => {
+     const {data} = await axios.get('/api/products');
+     setProducts(data)
+   };
+   fetchData()
+  }, [])
+
+
   return (
     <div>
       <div className="row center">
