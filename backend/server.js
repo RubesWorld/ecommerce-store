f1a9a5a92334ca3703985da926b0f9a76e6017e8
RubesWorld <hi@rubesworld.live>
2022-01-30 00:25:47 -0800

    added async package

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,10 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
